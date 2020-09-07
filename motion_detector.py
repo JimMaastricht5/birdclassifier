1055fcb99d19ab27220eb75ad02a8eb72f710ba7
JimMaastricht5 <JimMaastricht5@gmail.com>
2020-09-07 11:55:38 -0500

    refactor tweeter helper functions, added motion detectoin helper, and stiched together all three

---
+++ motion_detector.py
@@ -0,0 +1,40 @@
+# code to handle motion detection for pyface and tweetercam
+# pass in opencv, capture settings, and first_img
+# first_img should be without motion.
+# compare gray scale image to first image.  If different than motion
+# return image captured, gray scale, guassian blured version, thresholds, first_img, and countours
+import cv2
+import imutils
+
+
+# capture first image and gray scale/blur for baseline motion detection
+def init(cv2, cap):
+    ret, img = cap.read()  # capture an image from the camera
+    img = cv2.flip(img, -1)  # mirror image; comment out if not needed for your camera
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert image to gray scale BGR for OpenCv recognition
+    graymotion = cv2.GaussianBlur(gray, (21, 21), 0)  # smooth out image for motion detection
+    return graymotion
+
+
+# once first image is captured call motion detector in a loop to find each subsequent image
+# compare image to first img; if different than motion
+# return image, gray scale, gray blur, threshold image, and contours
+def detect(cv2, cap, first_img):
+    ret, img = cap.read()  # capture an image from the camera
+    img = cv2.flip(img, -1)  # mirror image; comment out if not needed for your camera
+    grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert image to gray scale BGR for OpenCv recognition
+    grayblur = cv2.GaussianBlur(grayimg, (21, 21), 0)  # smooth out image for motion detection
+
+    # motion detection
+    # compute the absolute difference between the current frame and first frame
+    imgdelta = cv2.absdiff(first_img, grayblur)
+    threshimg = cv2.threshold(imgdelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    # dilate the thresholded image to fill in holes, then find contours
+    # on thresholded image
+    threshimg = cv2.dilate(threshimg, None, iterations=2)
+    cnts = cv2.findContours(threshimg.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    return img, grayimg, grayblur, threshimg, cnts
--- pyface.py
+++ pyface.py
@@ -8,13 +8,21 @@
 # A: The pip install has been giving readers troubles since OpenCV 4.1.1
 # (around the November 2019 timeframe). Be sure to install version 4.1.0.25 :
 # packages: PCA9685-Driver, opencv-python=4.1.0.25
-import cv2
-import PanTilt9685
+import cv2  # opencv2
+import PanTilt9685  # pan tilt control code
+import motion_detector  # motion detector helper functions
+import tweeter  # twitter helper functions
 import imutils  # CV helper functions
 import argparse  # arguement parser
+# from twython import Twython; needed?
+from auth import (
+    api_key,
+    api_secret_key,
+    access_token,
+    access_token_secret
+)
 
-
-def face_detector(args)
+def face_detector(args):
     # setup pan tilt and initialize variables
     currpan, currtilt, pwm = PanTilt9685.init_pantilt()
     SCR_W = 640
@@ -24,29 +32,15 @@ def face_detector(args)
     cap = cv2.VideoCapture(0)  # capture video image
     cap.set(3, SCR_W)  # set Width
     cap.set(4, SCR_H)  # set Height
-    first_img = None  # init first img
+
+    twitter = tweeter.init(api_key, api_secret_key, access_token, access_token_secret)
+    first_img = motion_detector.init(cv2, cap)
 
     print('press esc to quit')
-
     while True:  # while escape key is not pressed
-        ret, img = cap.read()  # capture an image from the camera
-        img = cv2.flip(img, -1)  # mirror image; comment out if not needed for your camera
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert image to gray scale BGR for OpenCv recognition
-        graymotion = cv2.GaussianBlur(gray, (21, 21), 0)  # smooth out image for motion detection
-        if first_img is None:
-            first_img = graymotion
+        ret, img, gray, graymotion, thresh, first_img, cnts = motion_detector.detect(cv2, cap, first_img)
 
-        # motion detection
-        # compute the absolute difference between the current frame and
-        # first frame
-        imgdelta = cv2.absdiff(first_img, graymotion)
-        thresh = cv2.threshold(imgdelta, 25, 255, cv2.THRESH_BINARY)[1]
-        # dilate the thresholded image to fill in holes, then find contours
-        # on thresholded image
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
+        cnts = imutils.grab_contours(cnts)  # set of contours showing motion
         # loop over the contours
         for c in cnts:
             # if the contour is too small, ignore it
@@ -55,14 +49,21 @@ def face_detector(args)
 
             # motion detected
             # compute the bounding box for the contour, draw motion on frame
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # (x, y, w, h) = cv2.boundingRect(c)
+            # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             # look for a face or other object if motion is detected
             faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+            for (x, y, w, h) in faces:
+                rect = (x, y, (x + w), (y + h))
+                # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.rectangle(img, rect, (0, 255, 0), 2)
+                face_img = img[int(x):int(x + w), int(y):int(y + w)]  # one of these two shoudl work?
+                face_img = img(rect)  # one of these two should work?
+                twitter.post_image("found face", face_img)
 
             # control pan title mechanism
-            # currpan, currtilt = PanTilt9685.trackobject(pwm, cv2, currpan, currtilt, img, gray, faces, SCR_H, SCR_W)
+            currpan, currtilt = PanTilt9685.trackobject(pwm, cv2, currpan, currtilt, img, gray, faces, SCR_H, SCR_W)
 
         cv2.imshow('video', img)
         cv2.imshow('gray', graymotion)
