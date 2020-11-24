db09ede6077dc2cdb9e5d30362ec8b2f14d64cc3
JimMaastricht5 <JimMaastricht5@gmail.com>
2020-11-24 09:46:11 -0600

    increased bird confidence to 64% to reduce false neg

--- birdclass.py
+++ birdclass.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     ap.add_argument('-om', "--objmodel", default='/home/pi/PycharmProjects/pyface2/ssd_mobilenet_v1_1_metadata_1.tflite')
     ap.add_argument('-p', '--objlabels',
                     default='/home/pi/PycharmProjects/pyface2/lite-model_ssd_mobilenet_v1_1_metadata_2_labelmap.txt')
-    ap.add_argument('-c', '--confidence', type=float, default=0.60)
+    ap.add_argument('-c', '--confidence', type=float, default=0.64)
     ap.add_argument('-bc', '--bconfidence', type=float, default=0.30)
     ap.add_argument('-m', '--modelfile', default='/home/pi/PycharmProjects/pyface2/mobilenet_tweeters.tflite',
                     help='.tflite model to be executed')
