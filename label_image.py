fdfc6e2e4cffe766e1e1759e041984d708d9e485
JimMaastricht5 <JimMaastricht5@gmail.com>
2020-11-27 15:14:55 -0600

    developed more complex species model; revised code to reference

--- label_image.py
+++ label_image.py
@@ -170,7 +170,8 @@ if __name__ == '__main__':
     parser.add_argument(
         '-m',
         '--model_file',
-        default='/home/pi/PycharmProjects/pyface2/mobilenet_tweeters.tflite',
+        # default='/home/pi/PycharmProjects/pyface2/mobilenet_tweeters.tflite',
+        default='/home/pi/PycharmProjects/pyface2/birdskgc-s-224-92.44.h5',
         help='.tensor bird classification model to be executed')
     parser.add_argument(
         '-om',
@@ -180,7 +181,8 @@ if __name__ == '__main__':
     parser.add_argument(
         '-l',
         '--label_file',
-        default='/home/pi/PycharmProjects/pyface2/class_labels.txt',
+        # default='/home/pi/PycharmProjects/pyface2/class_labels.txt',
+        default='/home/pi/PycharmProjects/pyface2/birdskgc-17.txt',
         help='name of file containing labels for bird classification model')
     parser.add_argument(
         '-ol',
