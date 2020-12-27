ccabaa4a5becabc0671174b47957295ea46d166e
JimMaastricht5 <JimMaastricht5@gmail.com>
2020-12-27 12:14:54 -0600

    google automl tflite model

--- birdclass.py
+++ birdclass.py
@@ -3,28 +3,7 @@
 # uses Haar Cascade for bird detection and tensorflow lite model for bird classification
 # tensor flow model built using tools from tensor in colab notebook
 # https://colab.research.google.com/drive/1taZ9JincTaZuZh_JmBSC4pAbSQavxbq5#scrollTo=D3i_6WSXjUhk
-# model trained to find birds common to feeders in WI using caltech bird dataset 2011
-# 017.Cardinal
-# 049.Boat_tailed_Grackle
-# 094.White_breasted_Nuthatch
-# 118.House_Sparrow
-# 129.Song_Sparrow
-# 300.American_Goldfinch
-# 301.Baltimore_Oriole
-# 302.Black-Capped_Chickadee
-# 303.Brown_Thrasher
-# 305.Common_Starling
-# 306.Dark_Eyed_Junco
-# 307.Eastern_Bluebird
-# 308.Eastern_Meadowlark
-# 309.Eastern_Towee
-# 310.Evening_Grosbeak
-# 312.House_Finch
-# 316.Purple_Finch
-# 317.Robin
-# 318.Tit_Mouse
-
-
+# model trained to find birds common to feeders in WI using caltech bird dataset 201
 # special notes when setting up the code on a rasberry pi 4 9/9/20
 # install supporting libraries for directions @: https://qengineering.eu/install-opencv-4.2-on-raspberry-pi-4.html
 # install OpenCv version 4.4+
@@ -59,19 +38,15 @@ def bird_detector(args):
         currpan, currtilt, pwm = PanTilt9685.init_pantilt()
 
     # initial video capture, screen size, and grab first image (no motion)
-    if args["image"] == "":
-        try:
-            cap = cv2.VideoCapture(0)  # capture video image
-        except:
-            print('camera failure on initial video capture, ending program')
-            logging.critical('camera failure on initial video capture, ending program')
-            return
-
-        cap.set(3, args["screenwidth"])  # set screen width
-        cap.set(4, args["screenheight"])  # set screen height
-        first_img = motion_detector.init(cv2, cap)
-    else:
-        first_img = cv2.imread(args["image"])  # testing code
+    try:
+        cap = cv2.VideoCapture(0)  # capture video image
+    except:
+        print('camera failure on initial video capture, ending program')
+        logging.critical('camera failure on initial video capture, ending program')
+        return
+    cap.set(3, args["screenwidth"])  # set screen width
+    cap.set(4, args["screenheight"])  # set screen height
+    first_img = motion_detector.init(cv2, cap)
 
     twitter = tweeter.init(api_key, api_secret_key, access_token, access_token_secret)  # init twitter api
 
@@ -85,17 +60,11 @@ def bird_detector(args):
 
     # main loop ******
     while True:  # while escape key is not pressed
-        if args["image"] == "":
-            try:
-                motionb, img, gray, graymotion, thresh = motion_detector.detect(cv2, cap, first_img, args["minarea"])
-            except:
-                logging.critical('camera failed on motion capture and compare of second image, ending program')
-                return
-
-        else:  # testing code
-            motionb = True
-            first_img = cv2.imread(args["image"])
-            img = cv2.imread(args["image"])
+        try:
+            motionb, img, gray, graymotion, thresh = motion_detector.detect(cv2, cap, first_img, args["minarea"])
+        except:
+            logging.critical('camera failed on motion capture and compare of second image, ending program')
+            return
 
         if motionb:  # motion detected.
             det_confidences, det_labels, det_rects = label_image.object_detection(args["confidence"], img,
@@ -195,10 +164,10 @@ if __name__ == "__main__":
 
     # species model setup
     ap.add_argument('-m', '--species_model',
-                    default='/home/pi/PycharmProjects/pyface2/birdspecies-s-224-93.15.tflite',
+                    default='/home/pi/PycharmProjects/pyface2/birdspecies_auto_ml_2020-12-26.tflite',
                     help='.tflite model to be executed')
     ap.add_argument('-l', '--species_labels',
-                    default='/home/pi/PycharmProjects/pyface2/birdspecies-13.txt',
+                    default='/home/pi/PycharmProjects/pyface2/birdspecies_automl_2020-12-26.txt',
                     help='name of file containing labels')
 
     # tensor flow input arguements
@@ -212,11 +181,6 @@ if __name__ == "__main__":
 
     # set pan tilt control model
     ap.add_argument('--panb', default=False, type=bool, help='activate pan tilt mechanism')
-
-    # input test image or use video
-    # ap.add_argument('-i', '--image', default='/home/pi/birdclass/cardinal.jpg',
-    #                                         help='image to be classified')
-    ap.add_argument('-i', '--image', default='', help='image to be classified')
 
     logging.basicConfig(filename='birdclass.log', format='%(asctime)s - %(message)s', level=logging.INFO)
     arguments = vars(ap.parse_args())
