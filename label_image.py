903881b627036b3111523e10c0995d32fd3e4401
JimMaastricht5 <JimMaastricht5@gmail.com>
2020-12-27 13:25:18 -0600

    google automl tflite model

--- label_image.py
+++ label_image.py
@@ -128,6 +128,7 @@ def set_label(img, labels, interpreter, input_mean, input_std):
 
     # print(cresult, lresult)
     # print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
+    cresult = cresult / 100
     return cresult, lresult  # highest confidence and best label
 
 
