133b574840284f7934ca46e32434aa25549e4006
JimMaastricht5 <JimMaastricht5@gmail.com>
2020-12-30 15:26:21 -0600

    revert to prior keras model,auto ml was crap....

--- birdclass.py
+++ birdclass.py
@@ -164,10 +164,10 @@ if __name__ == "__main__":
 
     # species model setup
     ap.add_argument('-m', '--species_model',
-                    default='/home/pi/PycharmProjects/pyface2/birdspecies_automl_2020-12-26.tflite',
+                    default='/home/pi/PycharmProjects/pyface2/birdspecies-s-224-93.15.tflite',
                     help='.tflite model to be executed')
     ap.add_argument('-l', '--species_labels',
-                    default='/home/pi/PycharmProjects/pyface2/birdspecies_automl_2020-12-26.txt',
+                    default='/home/pi/PycharmProjects/pyface2/birdspecies-13.txt',
                     help='name of file containing labels')
 
     # tensor flow input arguements
