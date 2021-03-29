f9cea340e306921855a000c84eebcacada3947ee
JimMaastricht5 <JimMaastricht5@gmail.com>
2021-03-29 15:56:21 -0500

    add weather module

--- weather.py
+++ weather.py
@@ -28,28 +28,28 @@
 from auth import (
     weather_key
 )
-from pyowm import OWM
-from pyowm.utils import config
-from pyowm.utils import timestamps
+import requests
 
-# ---------- FREE API KEY examples ---------------------
+# pass in city name or default to sun prairie, WI
+def local_weather(city='Sun&Prairie'):
+    base_url = 'http://api.openweathermap.org/data/2.5/weather?q='
+    full_url= base_url + city +'&appid=' + weather_key
+    response = requests.get(full_url)
+    weather = str(response.json()['weather'])
+    print(weather)
+    start = weather.find('main')+ 8
+    skycondition = weather[start: start + 5]
+    if skycondition == 'Clear':
+        isclear = True
+    else:
+        isclear = False
+    return isclear
 
-owm = OWM(weather_key)
-mgr = owm.weather_manager()
+
+def main():
+    print(local_weather())
 
 
-# Search for current weather in London (Great Britain) and get details
-observation = mgr.weather_at_place('London,GB')
-w = observation.weather
-
-w.detailed_status         # 'clouds'
-w.wind()                  # {'speed': 4.6, 'deg': 330}
-w.humidity                # 87
-w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-w.rain                    # {}
-w.heat_index              # None
-w.clouds                  # 75
-
-# Will it be clear tomorrow at this time in Milan (Italy) ?
-forecast = mgr.forecast_at_place('Milan,IT', 'daily')
-answer = forecast.will_be_clear_at(timestamps.tomorrow())
+# test function
+if __name__ == '__main__':
+    main()
