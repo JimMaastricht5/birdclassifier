5ab8daa30b5f6a3e1977241cffc95b36e49050ab
jimmaastricht <jimmaastricht5@gmail.com>
2021-08-12 15:11:04 -0500

    correct requirements and setup to avoid installing larger tensorflow package

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 # PCA9685 needs to get added for pan tilt if you are using that.
+# tensorflow package is required for windows, tflite-runtime is the correct lib for rasp pi
 
 setup(
     name='Bird Class Python App',
     version='1.0',
-    packages=['cv2, scipy, requests, imutils, opencv-python, numpy, oauthlib, tensorflow, tflite-runtime, tywthon, Pillow, argparse, datetime, logging, PIL, scikit-image'],
+    packages=['cv2, scipy, requests, imutils, opencv-python, numpy, oauthlib, tflite-runtime,'
+              'tywthon, Pillow, argparse, datetime, logging, PIL, scikit-image'],
     url='',
     license='',
     author='maastricht',
