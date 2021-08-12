efcf8778baf239ebfc2f88b4a27f22711c523027
jimmaastricht <jimmaastricht5@gmail.com>
2021-08-12 18:52:01 -0500

    attempt to fix opencv numpy error with forced import of numpy multiarray before opencv2 import

--- label_image.py
+++ label_image.py
@@ -34,8 +34,9 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-import cv2
+import numpy.core.multiarray
 import numpy as np
+import cv2
 import math
 import random
 import image_proc
--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@ from setuptools import setup
 # PCA9685 needs to get added for pan tilt if you are using that.
 # tensorflow package is required for windows, tflite-runtime is the correct lib for rasp pi
 
+# from https://www.tensorflow.org/lite/guide/python
+# If you're running Debian Linux or a derivative of Debian (including Raspberry Pi OS),
+# you should install from our Debian package repo. This requires that you add a new repo list
+# and key to your system and then install as follows:
+# echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install python3-tflite-runtime
+
 setup(
     name='Bird Class Python App',
     version='1.0',
