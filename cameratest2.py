b475cae22ceac81281285491b973bdfa4474cb39
jimmaastricht <jimmaastricht5@gmail.com>
2021-08-16 21:51:47 -0500

    test camera code

--- cameratest2.py
+++ cameratest2.py
@@ -2,9 +2,9 @@ import picamera
 from picamera import PiCamera
 from time import sleep
 
-with picamera.PiCamera() as camera:
-    # camera = PiCamera()
-    camera.start_preview()
-    sleep(2)
-    camera.capture('testcap.jpg')
-    camera.stop_preview()
+# with picamera.PiCamera() as camera:
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+camera.capture('testcap2.jpg')
+camera.stop_preview()
