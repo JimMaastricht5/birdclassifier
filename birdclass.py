82d2f477d28819cb2858c2cdd12a70e550f7f719
jimmaastricht <jimmaastricht5@gmail.com>
2021-12-15 19:08:26 -0600

    improved wait time until sun rise feature.  waits exact  number of seconds to instead of waiting 30 minutes and rechecking

--- birdclass.py
+++ birdclass.py
@@ -50,9 +50,10 @@ def bird_detector(args):
 
     print(f'It is now {datetime.now()}.  \nSunrise at {cityweather.sunrise} and sunset at {cityweather.sunset}.')
     # wait here until the sun is up before initialize the camera
-    while (cityweather.sunrise.time() < datetime.now().time() < cityweather.sunset.time()) is False:
-        print('taking a 30 minutes nap to wait for sun rise or the end of the day')
-        time.sleep(1800)  # wait 30 minutes and check again to see if the sun is up
+    while cityweather.sunrise.time() < datetime.now().time() is False:
+        waittime = cityweather.sunrise.time() - datetime.now.time()
+        print(f'taking a {waittime.total_seconds} second to wait for sun rise')
+        time.sleep(waittime.total_seconds)  # wait until the sun comes up
 
     # initial video capture, screen size, and grab first image (no motion)
     camera, first_img = motion_detector.init(args)  # set gray motion mask
