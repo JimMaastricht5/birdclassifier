67f6f4e2e38aba654ed42f202662f856ddfde1c1
jimmaastricht <jimmaastricht5@gmail.com>
2022-02-22 08:24:00 -0600

    add debuging code for animated gif 30% with no label error

--- label_image.py
+++ label_image.py
@@ -182,9 +182,10 @@ class DetectClassify:
                 self.classified_labels.append(classify_label)
                 self.classified_confidences.append(classify_conf)
                 self.classified_rects.append(rect)
-        print('max confidence rounded', round(max(self.classified_confidences, default=0), 2))
+        print('max confidence rounded', max(self.classified_confidences, default=0))
         if round(max(self.classified_confidences, default=0), 2) == 0:  # if empty list zero or round and check
-            max_confidence = 0
+            # max_confidence = 0
+            pass
         else:
             max_confidence = max(self.classified_confidences)
             self.last_known_classified_rects = self.classified_rects
