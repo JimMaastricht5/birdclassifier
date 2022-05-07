e300c515ef53caca9030cdc2c53e6e1ac95480b3
jimmaastricht <jimmaastricht5@gmail.com>
2022-05-07 13:36:22 -0500

    started integrating web stream

--- dashapp.py
+++ dashapp.py
@@ -77,27 +77,63 @@ import os
 #     jpg_list = [file_name for file_name in file_list if ".jpg" in file_name]
 #     gif_list = [file_name for file_name in file_list if ".gif" in file_name]
 #     return jpg_list, gif_list
+#
+# from dash import Dash, dcc, html, Input, Output
+#
+# app = Dash(__name__)
+#
+# app.layout = html.Div([
+#     html.H6("Change the value in the text box to see callbacks in action!"),
+#     html.Div([
+#         "Input: ",
+#         dcc.Input(id='my-input', value='initial value', type='text')
+#     ]),
+#     html.Br(),
+#     html.Div(id='my-output'),
+#
+# ])
+#
+#
+# @app.callback(
+#     Output(component_id='my-output', component_property='children'),
+#     Input(component_id='my-input', component_property='value')
+# )
 
 from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "Input: ",
-        dcc.Input(id='my-input', value='initial value', type='text')
-    ]),
-    html.Br(),
-    html.Div(id='my-output'),
-
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        df['year'].min(),
+        df['year'].max(),
+        step=None,
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        id='year-slider'
+    )
 ])
 
 
 @app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
-)
+    Output('graph-with-slider', 'figure'),
+    Input('year-slider', 'value'))
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name="country",
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
 def update_output_div(input_value):
     return f'Output: {input_value}'
 if __name__ == "__main__":
