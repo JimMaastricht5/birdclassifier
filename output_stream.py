04fe8ab2184f09cf9191b194b8a7098ce6d97af0
jimmaastricht <jimmaastricht5@gmail.com>
2022-05-08 17:33:26 -0500

    first attempt at multiprocessing for output terminal and web

--- output_stream.py
+++ output_stream.py
@@ -30,7 +30,7 @@ class WebStream:
     def request_handler(self):
         while True:
             item = self.queue.get()  # get the next item in the queue to write to disk
-            print(item)
+            print('web process receiving:', item)
             if item is None:  # poison pill, end the process
                 break
             elif item[1] == 'flush':  # event type is flush
@@ -61,7 +61,7 @@ class Controller:
         return
 
     def message(self, message, event_num=0, msg_type='message', image_name=''):
-        print(message)
+        print('web controller sending: ', message)
         item = [event_num, msg_type, datetime.datetime.now().strftime("%H:%M:%S"), message, image_name]
         self.queue.put(item)
         return
