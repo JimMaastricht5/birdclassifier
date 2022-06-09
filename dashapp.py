e2fd192a5a8f8c645b794231dd5bc55b191c83fb
jimmaastricht <jimmaastricht5@gmail.com>
2022-06-09 11:53:20 -0500

    try image in table

--- dashapp.py
+++ dashapp.py
@@ -17,8 +17,7 @@ def load_message_stream():
     df = df.sort_values(by='Date Time', ascending=False)
     # Markdown format for image as a link: [![alt text](image link)](web link)
     df['Image Name'] = df['Image Name'].str[-5:]  # drop all but name of file 0.jpg
-    asset_base_url = app.get_asset_url('birds.gif')[0:-9]
-    df['Image Name'] = '[[alt text](http://' + asset_base_url + df['Image Name'] + ')](' + asset_base_url + ')'
+    df['Image Name'] = f'[![{df["Image Name"]}(/assets/' + df['Image Name'] + ')](/assets/' + ')'
     # df['Image Name'] = f"[![{df['Image Name']}](http:/{df['Image Name']})](http:/{df['Image Name']})"
     df = df[df['Event Num'] != 0]
     return df
