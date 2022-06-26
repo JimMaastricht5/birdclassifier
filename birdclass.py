25b26aa25a23241f6ae08e30248c3ce73e91550a
jimmaastricht <jimmaastricht5@gmail.com>
2022-06-26 16:26:46 -0500

    test lower entropy to increase motion with 8mp camera

--- birdclass.py
+++ birdclass.py
@@ -278,7 +278,7 @@ if __name__ == "__main__":
     # prediction defaults
     ap.add_argument("-co", "--default_confidence", type=float, default=.979, help="confidence threshold")
     ap.add_argument("-op", "--overlap_perc_tolerance", type=float, default=0.8, help="% box overlap to flag as dup")
-    ap.add_argument("-ma", "--minarea", type=float, default=4.5, help="motion entropy threshold")  # lower = > motion
+    ap.add_argument("-ma", "--minarea", type=float, default=4.0, help="motion entropy threshold")  # lower = > motion
 
     ap.add_argument("-hd", "--homedir", type=str, default='/home/pi/PycharmProjects/birdclassifier/',
                     help="home directory for files")
