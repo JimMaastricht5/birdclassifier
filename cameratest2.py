ba7a9348bfaba7c9eb28fde116f17b5a9831718f
jimmaastricht <jimmaastricht5@gmail.com>
2022-07-01 14:38:41 -0500

    camera test at 720 x 640

--- cameratest2.py
+++ cameratest2.py
@@ -2,11 +2,11 @@ from picamera import PiCamera
 from time import sleep
 import os
 
-screenheight = 720
-screenwidth = 640
+# screenheight = 720
+# screenwidth = 640
 camera = PiCamera()
 camera.vflip = False
-camera.resolution = (screenheight, screenwidth)
+# camera.resolution = (screenheight, screenwidth)
 camera.start_preview()
 sleep(2)
 camera.capture(os.getcwd()+ '/assets/testcap2.jpg')
