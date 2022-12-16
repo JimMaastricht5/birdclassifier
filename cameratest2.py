39d1a9734ee3919f78e00a870eafa727251b64b8
jimmaastricht <jimmaastricht5@gmail.com>
2022-12-16 13:15:27 -0600

    iso in camera test

--- cameratest2.py
+++ cameratest2.py
@@ -3,11 +3,11 @@ from time import sleep
 import os
 
 # 1024 x 768 or 720 x 640
-screenheight = 720
-screenwidth = 640
+screenheight = 640
+screenwidth = 480
 camera = PiCamera()
 camera.vflip = False
-#camera.iso = 800
+camera.iso = 800
 camera.resolution = (screenheight, screenwidth)
 camera.start_preview()
 sleep(2)
@@ -19,7 +19,7 @@ print(camera.framerate_range.high)
 print(camera.shutter_speed)
 
 #camera.shutter_speed=17  # 17 ms is 1/60 of a second
-camera.iso=800
+# camera.iso=800
 print(camera.framerate_range.low)
 print(camera.framerate_range.high)
 print(camera.shutter_speed)
