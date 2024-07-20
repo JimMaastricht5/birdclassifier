b2a011cda9bf8dd429e0d899e11bd43cb126c0ed
jimmaastricht <jimmaastricht5@gmail.com>
2024-07-20 09:52:59 -0500

    modify code to work with new picamera2 apis

--- cameratest2.py
+++ cameratest2.py
@@ -20,28 +20,63 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
+# old picamera 1 code
+# 1024 x 768 or 720 x 640
+# screenheight = 640
+# screenwidth = 480
+# camera = PiCamera()
+# camera.vflip = False
+# camera.resolution = (screenheight, screenwidth)
+# camera.start_preview()
+# sleep(2)
+# camera.capture(os.getcwd()+ '/assets/testcap2.jpg')
+# camera.stop_preview()
+#
+# print(camera.framerate_range.low)
+# print(camera.framerate_range.high)
+# print(camera.shutter_speed)
+# print(camera.exposure_speed)
+# print(camera.exposure_mode)
+
 from picamera2 import Picamera2
 from time import sleep
 import os
 
-# 1024 x 768 or 720 x 640
 screenheight = 640
 screenwidth = 480
+
 camera = Picamera2()
 camera.vflip = False
-#camera.exposure_mode = 'night'  # sports, off
-#camera.iso = 800
-camera.resolution = (screenheight, screenwidth)
-camera.start_preview()
-sleep(2)
-camera.capture(os.getcwd()+ '/assets/testcap2.jpg')
-camera.stop_preview()
 
-print(camera.framerate_range.low)
-print(camera.framerate_range.high)
-print(camera.shutter_speed)
-print(camera.exposure_speed)
-print(camera.exposure_mode)
+# Configure preview resolution
+config = picamera2.preview.preview_configuration(
+    size=(screenwidth, screenheight)
+)
+
+# Create preview stream (optional, comment out if not needed)
+preview_stream = camera.create_preview_stream(config)
+# preview_stream.start()  # Uncomment to show preview
+sleep(2)  # Adjust sleep duration as needed
+
+# Create capture request
+capture_request = camera.create_still_capture_request(config)
+
+# Capture image
+camera.capture(capture_request)
+
+# Save captured image
+with camera.capture_file(os.getcwd() + '/assets/testcap2.jpg') as file:
+    file.write(camera.capture_buffer)
+
+# Information printing (modify as needed based on picamera2 documentation)
+print("Frame rate range:", camera.framerate_range)  # Might require different method
+# ... Consult documentation for other properties ...
+
+# Stop preview if started
+# preview_stream.stop()
+
+camera.close()  # Close camera resources
+
 
 
 
