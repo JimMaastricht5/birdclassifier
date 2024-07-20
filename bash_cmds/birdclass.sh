8f5f0f0a7aac47297ebf83d53f282052146d1aef
jimmaastricht <jimmaastricht5@gmail.com>
2024-07-20 13:04:09 -0500

    modify bash command to activate venv

--- bash_cmds/birdclass.sh
+++ bash_cmds/birdclass.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~/birdclassifier
 git pull
+source venv/bin/activate
 python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv'
 
