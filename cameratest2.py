7cf04920da03c4662405a4e2a10d55a38d52b8ff
jimmaastricht <jimmaastricht5@gmail.com>
2024-07-30 08:36:31 -0500

    drop species of doves not found in wi; removed pine siskin from list as it never comes to the feeder and confuses the AI

--- cameratest2.py
+++ cameratest2.py
@@ -20,23 +20,40 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
+import argparse  # argument parser
 from time import sleep
 import os
 from picamera2 import Picamera2, Preview
 from libcamera import Transform  # add import to test setup for motion detector
 
-screenheight = 480
-screenwidth = 640
-picam2 = Picamera2()
 
-config = picam2.create_preview_configuration(main={"size": (screenheight, screenwidth)})
-picam2.configure(config)
-picam2.start_preview(Preview.QT)  # QTGL does not work properly.
-picam2.start()
-sleep(2)  # let the camera settle
-metadata = picam2.capture_file(os.getcwd()+ '/assets/testcap2.jpg')
-print(metadata)
+def camera_test(args):
+    screenheight = 480
+    screenwidth = 640
+    picam2 = Picamera2()
 
-picam2.close()
+    config = picam2.create_preview_configuration(main={"size": (screenheight, screenwidth)})
+    picam2.configure(config)
+    picam2.start_preview(Preview.QT)  # QTGL does not work properly.
+    picam2.start()
+    sleep(args.sleep_time)  # let the camera settle
+    metadata = picam2.capture_file(os.getcwd() + '/assets/testcap2.jpg')
+    print(metadata)
+
+    picam2.close()
+    return
 
 
+if __name__ == "__main__":
+    # construct the argument parser and parse the arguments
+    # load settings from config file to allow for simple override
+    ap = argparse.ArgumentParser()
+    # camera settings
+    ap.add_argument("-fc", "--flipcamera", type=bool, default=False, help="flip camera image")
+    ap.add_argument("-sw", "--screenwidth", type=int, default=640, help="max screen width")
+    ap.add_argument("-sh", "--screenheight", type=int, default=480, help="max screen height")
+
+    # general app settings
+    ap.add_argument("-st", "--sleep_time", type=int, default=2, help="time to hold preview")
+
+    camera_test(args)
