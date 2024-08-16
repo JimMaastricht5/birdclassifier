7d4ff5b5c69dfcb788f03bf3615a71ee823368b5
jimmaastricht <jimmaastricht5@gmail.com>
2024-08-16 16:42:15 -0500

    debugging pi

--- bash_cmds/birdclass_offline.sh
+++ bash_cmds/birdclass_offline.sh
@@ -2,4 +2,5 @@
 cd ~/birdclassifier
 git pull origin dev
 source venv/bin/activate
-python3 /home/pi/birdclassifier/cameratest2.py
+source venv/bin/activate
+python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv' --offline True
