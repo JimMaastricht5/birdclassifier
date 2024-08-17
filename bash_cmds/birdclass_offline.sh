2f483f8c44e23f6dd22b37ffa2e6975244b1c024
jimmaastricht <jimmaastricht5@gmail.com>
2024-08-17 13:23:12 -0500

    set debug flag for offline to false to min ouput and improve performance

--- bash_cmds/birdclass_offline.sh
+++ bash_cmds/birdclass_offline.sh
@@ -3,4 +3,4 @@ cd ~/birdclassifier
 git pull origin dev
 source venv/bin/activate
 source venv/bin/activate
-python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv' --offline True  --debug True
+python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv' --offline True  --debug False
