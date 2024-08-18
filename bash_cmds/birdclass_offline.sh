f908ad24f0fd9ce90cbfa9d8986f04a9374d2bad
jimmaastricht <jimmaastricht5@gmail.com>
2024-08-18 09:30:22 -0500

    minor correction to bash scripts, remove git pull from offline bash script for dev branch testing

--- bash_cmds/birdclass_offline.sh
+++ bash_cmds/birdclass_offline.sh
@@ -21,8 +21,7 @@ echo $$ > "$PIDFILE"
 
 # Your script logic here
 cd ~/birdclassifier
-git pull origin dev
-source venv/bin/activate
+# git pull origin dev
 source venv/bin/activate
 python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv' --offline True  --debug False
 
