a1fda5619a3760deeceb23b8b7f319604b8eef35
jimmaastricht <jimmaastricht5@gmail.com>
2024-08-18 08:47:48 -0500

    set debug flag for offline to false to min ouput and improve performance

--- bash_cmds/birdclass_offline.sh
+++ bash_cmds/birdclass_offline.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+
+PIDFILE="/var/run/birdclass.pid"
+
+function cleanup() {
+  rm -f "$PIDFILE"
+  # Other cleanup actions
+}
+
+trap cleanup TERM INT
+
+if [ -f "$PIDFILE" ]; then
+  PID=$(cat "$PIDFILE")
+  if kill -0 $PID > /dev/null 2>&1; then
+    echo "Previous script is running. Terminating it gracefully..."
+    kill $PID
+  fi
+fi
+
+echo $$ > "$PIDFILE"
+
+# Your script logic here
 cd ~/birdclassifier
 git pull origin dev
 source venv/bin/activate
 source venv/bin/activate
 python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv' --offline True  --debug False
+
+# clean up
+cleanup
