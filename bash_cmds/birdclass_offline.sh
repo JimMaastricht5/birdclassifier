8865cbf9be2016c123d74444f1b89291dc2f115d
jimmaastricht <jimmaastricht5@gmail.com>
2024-12-26 08:00:43 -0600

    reduce threshold for offline testing for birds

--- bash_cmds/birdclass_offline.sh
+++ bash_cmds/birdclass_offline.sh
@@ -2,4 +2,4 @@
 cd ~/birdclassifier
 git pull origin dev
 source venv/bin/activate
-python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv' --offline True  --debug True --bird_confidence 0.3
+python3 /home/pi/birdclassifier/birdclass.py --homedir '/home/pi/birdclassifier/' --thresholds 'USA_WI_coral.ai.inat_bird_threshold.csv' --offline True  --debug True --bird_confidence 0.3 --minimgperc 5.0
